2f32c46064e9222eaf103bfafbc061588a8f7194
hendra <hengunadi@cecs-042952.cecs.anu.edu.au>
2019-09-17 17:29:10 +1000

    Fill the CSV files with no bugs. Used when there is incomplete triage result that does not include non-bug

---
+++ plot/fill_non_bugs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# take the filename of the actual crashes and a bug report,
+# this script will fill in the rest of the crashes with no bug ID ("")
+
+BASE_DIR=$1
+BUG_FILE=$2
+OUT_FILE=$3
+
+echo "experiment_type,trial_number,fuzzer_id,crash_id,bug_id" > ${OUT_FILE}
+
+for exp in $(ls -1 ${BASE_DIR}); do 
+  if [ "${exp}" == "stack_hashes" ]; then continue; fi
+  EXP_TYPE=${exp%_*}
+  TRIAL_NUMBER=${exp##*_}
+  echo "${exp} - ${EXP_TYPE} ${TRIAL_NUMBER}"
+  for fuzzer in $(ls -1 ${BASE_DIR}/${exp}); do
+    FUZZ_ID=$(echo ${fuzzer:6} | awk '{printf "%d\n",$0;}')
+    for f in $(ls -1 ${BASE_DIR}/${exp}/${fuzzer}/crashes | grep "id:"); do
+      CRASH_ID=$(echo ${f%%,*} | sed 's/id://' | awk '{printf "%d\n",$0;}')
+      BUG_ID=$(grep "${EXP_TYPE},${TRIAL_NUMBER},${FUZZ_ID},${CRASH_ID}," ${BUG_FILE} | cut -d, -f5)
+      if [ -z "${BUG_ID}" ]; then BUG_ID=""; fi
+      echo "${EXP_TYPE},${TRIAL_NUMBER},${FUZZ_ID},${CRASH_ID},${BUG_ID}" >> ${OUT_FILE}
+    done
+  done
+done

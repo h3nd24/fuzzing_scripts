493fd4fb3ac454ac8b7cf5f2cdaaa557e267a317
hendra <hengunadi@cecs-042952.cecs.anu.edu.au>
2019-09-18 16:01:47 +1000

    Simple script to help compare the number of bugs across files that contains the number of bugs per trial

---
+++ plot/compare_result.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Simple script to compare the number of bugs (result of time_to_find.py, default to "_AUC" files)
+
+PROG=${1}
+INFIX=${2}
+LEFT=${3}
+RIGHT=${4}
+
+paste -d"\t" <(cut -d"," -f2 plot_data_stats/${PROG}_18h_${INFIX}_${LEFT}_AUC) <(cut -d"," -f2 plot_data_stats/${PROG}_18h_${INFIX}_${RIGHT}_AUC)

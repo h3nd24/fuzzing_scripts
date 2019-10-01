e23da67daf84a79e8e4e34e7dc8cdb425bc32574
hendra <hendra.gunadi@anu.edu.au>
2019-10-01 11:49:01 +1000

    Script to create a copy of existing corpus and add empty seed to the corpus

---
+++ corpus/add_empty_to_corpus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Simple script to create another corpus from existing corpus + empty seed.
+# This is mainly used to the corpus generation of the experiments of adding empty seed into the corpus
+
+EMPTY_DIR=${1}
+SOURCE_DIR=${2}
+TARGET_DIR=${3}
+FILE_EXTENSION=${4}
+
+mkdir ${TARGET_DIR}
+ln -v ${SOURCE_DIR}/* ${TARGET_DIR}/
+
+echo "Adding empty"
+# There should be only one file in the empty directory, otherwise it does not make any sense.
+md5sum ${EMPTY_DIR}/* | awk -v td="${TARGET_DIR}" -v ext="${FILE_EXTENSION}" '{system("ln -v "$2" "td"/"$1""ext)}'
